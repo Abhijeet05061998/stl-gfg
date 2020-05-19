b49a8948442378ad433330419f42549b504e5762
Abhijeet Kumar <44672451+Abhijeet05061998@users.noreply.github.com>
2020-05-19 22:33:06 +0530

    Add files via upload

---
+++ multiset.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<set>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    multiset<int>s;
+    for(int i=0;i<n;i++)
+    {
+        int num;
+        cin>>num;
+        s.insert(num);
+    }
+    multiset<int>::iterator it;
+    for(it=s.begin();it!=s.end();it++)
+    {
+        cout<<*it<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
+
